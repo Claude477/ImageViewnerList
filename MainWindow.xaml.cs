709dab1df85958c0718e4709c00abc13ae86d3bf
Claude477 <esa.korkiakoski@gmail.com>
2016-10-18 18:48:51 +0300

    file browser with image view
    file browser with image view each when puss images

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace gifview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow() { InitializeComponent(); }
+        public string[] Files{ get; set; }
+
+        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.ShowDialog(); //show dialog
+            string polku = System.IO.Path.GetDirectoryName(ofd.FileName);
+            Files = System.IO.Directory.GetFiles(polku);
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (App.Args.Length > 0)
+            {
+                string polku = System.IO.Path.GetDirectoryName(App.Args[0]);
+                Files = System.IO.Directory.GetFiles(polku);
+                this.DataContext = this;
+            }
+            else
+            {
+            }  
+            
+        }
+    }
+}
